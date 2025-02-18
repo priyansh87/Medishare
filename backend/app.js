28cf847ef8a6d46e152e319b4114225683299e45
Priyansh87 <priyansh56701@gmail.com>
2025-02-18 20:05:38 +0530

    db connection , backend setup  , user model created

---
+++ backend/app.js
@@ -0,0 +1,32 @@
+import express from "express"  ;
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv" ;
+import cors from "cors";
+import morgan from "morgan";
+dotenv.config() ; 
+
+
+const app = express() 
+
+app.use(
+    cors({
+      origin: "http://localhost:5173",
+      methods: ["GET", "POST", "PUT", "DELETE"],
+      allowedHeaders: ["Content-Type", "Authorization"],
+      credentials: true,
+    })
+);
+
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(morgan("dev"));
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+
+export default app;
+
+
